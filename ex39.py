c62a9f837cc3bfd32e7e9d6fe7e343bc177800a7
alycolas <alycoals@gmail.com>
2017-09-22 22:47:21 +0800

    dict use {} not []

--- ex39.py
+++ ex39.py
@@ -1,18 +1,18 @@
 # create a mapping of state to abbreviation
-states = [
+states = {
 	'Oregon': 'OR',
 	'Florida': 'FL',
 	'California': 'CA',
 	'New York': 'NY',
 	'Michigan': 'MI'
-]
+}
 
 # create a basic set of states and some cities in them
-cities = [
+cities = {
 	'CA': 'San Francisco',
 	'MI': 'Detroit',
 	'FL': 'Jacksonville',
-]
+}
 
 # add some more cities
 cities['NY'] = 'New York'
@@ -30,8 +30,8 @@ print "Floria's abbreviation is:", states['Florida']
 
 # do it by using the state then cities dict
 print '_' * 10
-print "Michigan has: ", cities[state['Michigan']]
-print "Florida has: ", cities[state['Florida']]
+print "Michigan has: ", cities[states['Michigan']]
+print "Florida has: ", cities[states['Florida']]
 
 # print every state abbreviation
 print '_' * 10
